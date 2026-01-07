a0d2f653db0d861e5dcfe7a32d038df49a0cda3e
John Apella <johncollons5@gmail.com>
2026-01-07 23:23:43 +0300

    review list slicing

--- basics/data_structures/lists/lists.py
+++ basics/data_structures/lists/lists.py
@@ -1,1 +1,24 @@
 # list comprehension
+nums = [3, 4, 5, 6, 7, 8]
+
+print("Slicing")
+
+# - including the first index until the end
+print(nums[0:])
+
+# - excluding the last index from the start
+print(nums[1:2])
+
+# - returning all
+print(nums[:])
+
+target = 10
+
+def find_target():
+    for a, b in enumerate(nums):
+        diff = target - b
+        if diff in nums[a + 1:]:
+            return [a, nums.index(diff)]
+
+print("Target Found")
+print(find_target())
